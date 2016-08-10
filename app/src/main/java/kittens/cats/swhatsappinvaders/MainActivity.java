1a88aeca141a38e03154629d2361be5ec0433f29
Aquaatic <aquaticvfx@gmail.com>
2016-08-10 11:03:13 +0200

    Set content view to GamePanel

--- app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main); (Set to Surfaceview once ready)
+        this.setContentView(new GamePanel(this));
     }
 }
