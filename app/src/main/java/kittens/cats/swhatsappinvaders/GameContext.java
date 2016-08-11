0afca944ed125e1e91c6214637fdb59991b15e69
Aquaatic <aquaticvfx@gmail.com>
2016-08-11 15:56:40 +0200

    Add GameContext#getGameContext() and stop Player from moving outside of the screen

--- app/src/main/java/kittens/cats/swhatsappinvaders/GameContext.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/GameContext.java
@@ -8,18 +8,24 @@ import kittens.cats.swhatsappinvaders.player.Player;
 public class GameContext {
 
     private static Player player;
-
-
+    private static GamePanel gamePanel;
 
     public static void setPlayer(Player playerObject){
         player = playerObject;
     }
 
+    public static void setGamePanel(GamePanel panel) {
+        GameContext.gamePanel = panel;
+    }
 
     public static Player getPlayer(){
 
         return player;
 
+    }
+
+    public static GamePanel getGamePanel() {
+        return gamePanel;
     }
 
 }
