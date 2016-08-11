1c9f7c88c61b459b861c18be8edafaba652d131d
Prolog <jovibe@me.com>
2016-08-11 15:29:14 +0200

    Health integration in Stats
    Stuff and Things, mostly things.

--- app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kittens.cats.swhatsappinvaders.enemies.NormalEnemy;
+import kittens.cats.swhatsappinvaders.player.Player;
 import kittens.cats.swhatsappinvaders.util.DoubleVector;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GamePanel panel = new GamePanel(this);
+        GamePanel panel = new GamePanel(new Player(this, 3), this);
         panel.addGameObject(new NormalEnemy(this, new DoubleVector(500, 500)));
-        panel.addGameObject(Stats.getStats(this));
         this.setContentView(panel);
 
 
