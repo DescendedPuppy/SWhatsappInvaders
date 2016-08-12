383e0777d87134cd8d48e90dcc5e0bec4bf6f565
AscendedKitten <kittenascension@gmail.com>
2016-08-12 10:11:05 +0200

    Finished Enemies, fixed some items

--- app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/MainActivity.java
@@ -19,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         GamePanel panel = new GamePanel(new Player(this, 3), this);
 
 
-        panel.addGameObject(new NormalEnemy(this, new DoubleVector(500, 500)));
+      //  panel.addGameObject(new NormalEnemy(this, new DoubleVector(500, 500)));
 
         //Random Tests
         panel.addGameObject(new Boss(this, new DoubleVector(500, 500)));
-        panel.addGameObject(new NormalEnemy(this, new DoubleVector(400, 750)));
+        //panel.addGameObject(new NormalEnemy(this, new DoubleVector(400, 750)));
 
 
-        panel.addGameObject(new NormalEnemy(this, new DoubleVector(500, 500)));
+        //panel.addGameObject(new NormalEnemy(this, new DoubleVector(500, 500)));
 
         this.setContentView(panel);
 
--- app/src/main/java/kittens/cats/swhatsappinvaders/enemies/Enemy.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/enemies/Enemy.java
@@ -2,11 +2,9 @@ package kittens.cats.swhatsappinvaders.enemies;
 
 
 import android.content.Context;
-import android.util.Log;
 
 import kittens.cats.swhatsappinvaders.Bullet;
 import kittens.cats.swhatsappinvaders.EntityType;
-import kittens.cats.swhatsappinvaders.GameContext;
 import kittens.cats.swhatsappinvaders.GameObject;
 import kittens.cats.swhatsappinvaders.MainThread;
 import kittens.cats.swhatsappinvaders.util.DoubleVector;
@@ -34,11 +32,12 @@ public abstract class Enemy extends GameObject {
     public void update() {
 
         if(getLocation().x <= 0 || getLocation().x >= tempCanvasWidth - getWidth()){
-        Log.d("Enemy", "Loc X: "+getLocation().x+" | TempCanvWidth: "+tempCanvasWidth+" | Width: "+getWidth());
+
             setSpeed(getSpeed() * -1);
 
             if(getLocation().x <= 0)
                 setLocation(new DoubleVector(1, getLocation().y + getHeight()));
+
             else if(getLocation().x >= tempCanvasWidth - getWidth())
                 setLocation(new DoubleVector(tempCanvasWidth - getWidth() - 3, getLocation().y + getHeight()));
 
