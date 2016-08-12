8222690a7fd41e3c5fba4a7ea5eb2b3b7966d77a
Nobody42 <nick258e@gmail.com>
2016-08-12 15:11:53 +0200

    Use a copy of the GameObjects list to iterate over them in GamePanel

--- app/src/main/java/kittens/cats/swhatsappinvaders/Stats.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/Stats.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
+import android.graphics.Rect;
 
 import kittens.cats.swhatsappinvaders.player.Player;
 import kittens.cats.swhatsappinvaders.util.DoubleVector;
@@ -24,6 +25,7 @@ public class Stats{
     private int score = 10000;
     private int lives;
     private int kills;
+    private double bettertopscreen;
 
     private double halfwidth;
     private double bottomscreen,topscreen;
@@ -57,22 +59,27 @@ public class Stats{
         bottomscreen = canvas.getHeight() - 50;
         topscreen = canvas.getHeight()/25;
         rightscreen = 50;
+        bettertopscreen = 50;
 
 
         Paint paint = new Paint();
         paint.setColor(Color.WHITE);
         paint.setTextAlign(Paint.Align.CENTER);
-        paint.setTextSize(32.0f);
+        paint.setTextSize(50.0f);
 
 
         canvas.drawText(""+score, (float)halfwidth, (float)topscreen, paint);
         Bitmap bm;
-        bm = BitmapFactory.decodeResource(null, R.drawable.spaceship);
+        bm = BitmapFactory.decodeResource(GameContext.getGamePanel().getResources(), R.drawable.spaceship);
         Paint trymypaint;
+        Rect rekt;
+        rekt = new Rect();
         trymypaint= new Paint();
+
         for (int i=0;i<=lives;i++)
         {
-            canvas.drawBitmap(bm ,canvas.getWidth()/10+(i*10),canvas.getWidth()/14,trymypaint);
+            rekt.set(i*50+10, (int)Math.ceil(bettertopscreen), i*50+50, (int)Math.ceil(bettertopscreen)+50);
+            canvas.drawBitmap(bm, null, rekt, trymypaint);
         }
 
     }
