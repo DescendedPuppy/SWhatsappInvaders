edeae62025db467191997240888ee6ec2d0f5b68
Aquaatic <aquaticvfx@gmail.com>
2016-08-13 20:05:50 +0200

    Improved Stats

--- app/src/main/java/kittens/cats/swhatsappinvaders/Stats.java
+++ app/src/main/java/kittens/cats/swhatsappinvaders/Stats.java
@@ -1,17 +1,13 @@
 package kittens.cats.swhatsappinvaders;
 
-import android.content.ClipData;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Picture;
 import android.graphics.Rect;
 
 import kittens.cats.swhatsappinvaders.player.Player;
-import kittens.cats.swhatsappinvaders.util.DoubleVector;
 
 /**
  * Ich bin Joris.
@@ -21,18 +17,6 @@ public class Stats{
     private static Stats stats;
 
     private Player player;
-
-    private int score = 10000;
-    private int lives;
-    private int kills;
-    private double bettertopscreen;
-
-    private double halfwidth;
-    private double bottomscreen,topscreen;
-    private double leftscreen;
-    private double rightscreen;
-
-    private String item;
 
     private Stats() {
         player = GameContext.getPlayer();
@@ -45,90 +29,37 @@ public class Stats{
         return Stats.stats;
     }
 
-
-
-
-
-
-
     public void render(Canvas canvas) {
+        int score = 100000; // TODO: get score from elsewhere
 
-        lives = player.getHealth();
+        int health = this.player.getHealth();
 
-        halfwidth = canvas.getWidth() / 2;
-        bottomscreen = canvas.getHeight() - 50;
-        topscreen = canvas.getHeight()/25;
-        rightscreen = 50;
-        bettertopscreen = 50;
+        double halfWidth = canvas.getWidth() / 2;
+        double topOffset = canvas.getHeight() / 75;
+
+        // Score
+
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(50F);
+
+        canvas.drawText(String.valueOf(score), (float) halfWidth, (float) topOffset + 50, textPaint);
 
 
-        Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
-        paint.setTextAlign(Paint.Align.CENTER);
-        paint.setTextSize(50.0f);
+        Bitmap bm = BitmapFactory.decodeResource(
+                GameContext.getGamePanel().getResources(), R.drawable.spaceship
+        );
 
+        Rect healthContainer = new Rect();
+        Paint healthPaint = new Paint();
 
-        canvas.drawText(""+score, (float)halfwidth, (float)topscreen, paint);
-        Bitmap bm;
-        bm = BitmapFactory.decodeResource(GameContext.getGamePanel().getResources(), R.drawable.spaceship);
-        Paint trymypaint;
-        Rect rekt;
-        rekt = new Rect();
-        trymypaint= new Paint();
-
-        for (int i=0;i<=lives;i++)
-        {
-            rekt.set(i*50+10, (int)Math.ceil(bettertopscreen), i*50+50, (int)Math.ceil(bettertopscreen)+50);
-            canvas.drawBitmap(bm, null, rekt, trymypaint);
+        for (int i = 0; i <= health; i++) {
+            healthContainer.set(i * 50 + 10, (int) topOffset, i * 50 + 50, (int) topOffset + 50);
+            canvas.drawBitmap(bm, null, healthContainer, healthPaint);
         }
 
     }
-
-    //SETTERS
-    public void setScore(int score){
-
-        this.score = score;
-
-    }
-    public void setLives(int lives){
-
-        this.lives = lives;
-
-    }
-    public void setKills(int kills){
-
-        this.kills = kills;
-
-    }
-    public void setItem(String item){
-
-        this.item = item;
-
-    }
-
-    //GETTERS
-    public int getScore(){
-
-        return this.score;
-
-    }
-    public int getLives(){
-
-        return this.lives;
-
-    }
-    public int getKills(){
-
-        return this.kills;
-
-    }
-    public String getItem(){
-
-        return this.item;
-
-    }
-
-
 
 }
 
